67c6dbee24a814210a38fabf3356819a60ede41d
Santiago Cano <sanblackx1@gmail.com>
2023-02-15 23:45:13 -0500

    NEW: Adding a Basic Scene on Canvas

---
+++ BasicScene/script.js
@@ -0,0 +1,39 @@
+const scene = new THREE.Scene();
+
+/**
+ * To create a visible object we create a MESH
+ * A MESH is a combination of Geometry and Materials
+ */
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// PROVIDED Camera
+
+const ar = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * @function PerspectiveCamera
+ * @param FOV - Focal View in degrees
+ * @param AspectRatio - Width / Height
+ * @param Renderer - Where to visualize the Scene from a Camera POV
+ */
+const camera = new THREE.PerspectiveCamera(75, ar.width / ar.height);
+camera.position.z = 4;
+camera.position.x = 1;
+camera.position.y = 1;
+
+scene.add(camera);
+
+const canvas = document.getElementById("webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(ar.width, ar.height);
+renderer.render(scene, camera);
---
+++ README.md
@@ -0,0 +1,7 @@
+# Learning THREE.js
+
+Each commit will contain a step by step progress of the Three.js Library, I'll hope to create innovated websites with the power of THREE 🌱
+
+## Lessons
+
+- BasicScene: Use of Scene, Camera and renderer to view a Red Cube ✨
